96be8d36da29868c5c53adcd0fbcfb4d7b4384d1
Mohd Naushad Ahmed <naushad.dln@gmail.com>
2023-05-24 00:09:33 +0530

    Updated

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -37,55 +37,49 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     ngOnInit(): void {
         if(!this.devices.length) {
-            //Get Device List as it's not available with us now.
-            if(!environment.production)
-                this.devices = [
-                    
-                ];
+            this.http.getDevices().subscribe((rsp: Array<string>) => {
+            //this.subject.emit_deviceList(rsp);
+            let serialNumber: string = "";
+            let machineName:string = "";
+            let ipAddress:string = "";
+            let productName:string = "";
+            let osVersion:string = "";
+            let osBuildnumber:string = "";
+            let osName:string = "";
 
-                this.subject.emit_deviceList(this.devices);
-            } else {
-                this.http.getDevices().subscribe((rsp: Array<string>) => {
-                  //this.subject.emit_deviceList(rsp);
-                  let serialNumber: string = "";
-                  let machineName:string = "";
-                  let ipAddress:string = "";
-                  let productName:string = "";
-                  let osVersion:string = "";
-                  let osBuildnumber:string = "";
-                  let osName:string = "";
-
-                  for(let offset:number = 0; offset < rsp.length; ++offset) {
-                    for(let idx: number = 0; idx < rsp[offset].length; ++idx) {
-                      let ent = JSON.stringify(rsp[offset][idx]);
-                      JSON.parse(ent, (key, value) => {
-                          if(key && key == "device.machine") {
-                              machineName = value;
-                          } else if(key && key == "device.provisioning.serial") {
-                              serialNumber = value;
-                          } else if(key && key == "net.interface.common[w1].ipv4.address") {
-                              ipAddress = value;
-                          } else if(key && key == "net.interface.common[w1].ipv4.connectivity") {
-                              //
-                         } else if(key && key == "device.product") {
-                             productName = value;
-                         } else if(key && key == "system.os.version") {
+            for(let offset:number = 0; offset < rsp.length; ++offset) {
+                for(let idx: number = 0; idx < rsp[offset].length; ++idx) {
+                    let ent = JSON.stringify(rsp[offset][idx]);
+                    JSON.parse(ent, (key, value) => {
+                        if(key && key == "device.machine") {
+                            machineName = value;
+                        } else if(key && key == "device.provisioning.serial") {
+                            serialNumber = value;
+                        } else if(key && key == "net.interface.common[w1].ipv4.address") {
+                            ipAddress = value;
+                        } else if(key && key == "net.interface.common[w1].ipv4.connectivity") {
+                            //
+                        } else if(key && key == "device.product") {
+                            productName = value;
+                        } else if(key && key == "system.os.version") {
                              osVersion = value;
-                         } else if(key && key == "system.os.name") {
+                        } else if(key && key == "system.os.name") {
                              osName = value;
-                         } else if(key && key == "system.os.buildnumber") {
+                        } else if(key && key == "system.os.buildnumber") {
                              osBuildnumber = value;
-                         }
-                      });
-                    }
-                    let elm = {"ipAddress" : ipAddress, "serialNumber" : serialNumber, "deviceName" : machineName, "isDeviceAvailable" : true, 
-                               "productName": productName, "osVersion": osVersion, "osBuildnumber": osBuildnumber, "osName": osName 
-                              };
-                    this.devices.push(elm);
-                  }
-                },
-                (error) => {},
-                () => {this.subject.emit_deviceList(this.devices)});
+                        }
+                    });
+                }
+                let elm = {"ipAddress" : ipAddress, "serialNumber" : serialNumber, "deviceName" : machineName, "isDeviceAvailable" : true, 
+                       "productName": productName, "osVersion": osVersion, "osBuildnumber": osBuildnumber, "osName": osName 
+                      };
+                this.devices.push(elm);
             }
+          },
+          (error) => {},
+          () => {this.subject.emit_deviceList(this.devices)});
+        } else {
+          //
+        }
     }
 }
